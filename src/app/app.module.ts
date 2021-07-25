689d02e7954958eb9b778669a4f166eff55b042f
MNaguib2 <mena_afefe3000@yahoo.com>
2021-07-25 02:15:16 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { basichighlightdirective } from './basic-highlight/basic-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    basichighlightdirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
